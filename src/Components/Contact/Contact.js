ede921205256294ca4822c82cffa1fe171bf2f47
Sumi Dey <sumidey446@gmail.com>
2023-03-11 19:41:45 +0530

    last commit

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -22,16 +22,19 @@ const Contact = () => {
             </div>
             <div className='right-contact'>
                 <div>
+                <div>
                     <form ref={form} className='email-container' onSubmit={sendEmail} >
                         <input type='text' className='input' placeholder='Your Name' /><br />
                         <input type='email' name='user-email' className='input' placeholder='Your Email' /><br />
                         <input type='text' className='input' placeholder='Your Phone' /><br />
-                        <button className='submit'>Submit</button>
+                        {/* <button className='submit'>Submit</button> */}
                     </form>
                 </div>
                 <div>
                     <textarea id="subject" name="subject" className='textarea' placeholder="Your messege" />
                 </div>
+                </div>
+                <div><button className='submit'>Submit</button></div>
             </div>
         </div>
     )
